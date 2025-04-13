d8b0b13879324d399c2e1f7463dee2219ae1b489
discotechnology <drosenfeld@csumb.edu>
2025-04-13 10:08:53 -0700

    Added new activity BattleActivity

---
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/BattleActivity.java
@@ -0,0 +1,13 @@
+package com.lutra.legallydistinctpocketmonsterarea;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BattleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle);
+    }
+}
