7731073de3ef3fb00f6c3aef647f768c58fc42f4
Angelica Ghazaryan <angelicaghazaryann@gmail.com>
2025-04-16 15:23:08 -0700

    Added AdminLobbyActivity.java

---
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/AdminLobbyActivity.java
@@ -0,0 +1,64 @@
+package com.lutra.legallydistinctpocketmonsterarea;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.lutra.legallydistinctpocketmonsterarea.databinding.ActivityAdminLobbyBinding;
+
+public class AdminLobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityAdminLobbyBinding binding  = ActivityAdminLobbyBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        binding.BattleB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Battle button is clicked", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+        binding.EditMonstersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Edit Monster button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.CreateMonsterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Create Monster button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.EditUsersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Edit user button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.CreateUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Create user button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.LogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AdminLobbyActivity.this, "Loggout  is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
