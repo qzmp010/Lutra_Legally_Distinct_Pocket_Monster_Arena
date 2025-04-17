43824dadeff727dfd714058c5a83023b9e719cc8
qzmp010 <66999686+qzmp010@users.noreply.github.com>
2025-04-17 09:15:11 -0700

    initial implementation of ViewMonstersActivity using recycler view

---
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/ViewMonstersActivity.java
@@ -0,0 +1,46 @@
+package com.lutra.legallydistinctpocketmonsterarea;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.lutra.legallydistinctpocketmonsterarea.database.AppRepository;
+import com.lutra.legallydistinctpocketmonsterarea.databinding.ActivityViewMonstersBinding;
+import com.lutra.legallydistinctpocketmonsterarea.viewHolders.MonsterAdapter;
+import com.lutra.legallydistinctpocketmonsterarea.viewHolders.MonsterAdapter.UserMonsterDiff;
+import com.lutra.legallydistinctpocketmonsterarea.viewHolders.MonsterViewModel;
+
+public class ViewMonstersActivity extends AppCompatActivity {
+
+  private ActivityViewMonstersBinding binding;
+  private AppRepository repository;
+  private MonsterViewModel monsterViewModel;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityViewMonstersBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    monsterViewModel = new ViewModelProvider(this).get(MonsterViewModel.class);
+
+    RecyclerView recyclerView = binding.viewMonstersRecyclerView;
+    final MonsterAdapter adapter = new MonsterAdapter(new UserMonsterDiff());
+    recyclerView.setAdapter(adapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    repository = AppRepository.getRepository(getApplication());
+
+    //todo: get data for specifc userId
+    monsterViewModel.getByUserIdLiveData(0).observe(this, adapter::submitList);
+  }
+
+  public static Intent intentFactory(Context context) {
+    Intent intent = new Intent(context, ViewMonstersActivity.class);
+    return intent;
+  }
+}
