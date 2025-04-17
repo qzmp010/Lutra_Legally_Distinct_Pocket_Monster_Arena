86875ff0cd7ec7883b19531833885db16562b9e3
Victor Gordian <victorgordian@hotmail.com>
2025-04-17 16:37:52 -0700

    added functionally for logout

--- app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MainActivity.java
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MainActivity.java
@@ -1,12 +1,20 @@
 package com.lutra.legallydistinctpocketmonsterarea;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.lutra.legallydistinctpocketmonsterarea.database.AppRepository;
 import com.lutra.legallydistinctpocketmonsterarea.database.entities.MonsterType;
+import com.lutra.legallydistinctpocketmonsterarea.database.entities.User;
 import com.lutra.legallydistinctpocketmonsterarea.database.entities.UserMonster;
 import com.lutra.legallydistinctpocketmonsterarea.databinding.ActivityMainBinding;
 import java.util.stream.Collectors;
@@ -16,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
   private ActivityMainBinding binding;
   private AppRepository repository;
 
-  int loggedInUserId = -1;
+  private int loggedInUserId = -1;
+  private User user;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     setContentView(binding.getRoot());
 
     loginUser();
+
+    invalidateOptionsMenu();
 
     if(loggedInUserId == -1 ){
       Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
@@ -45,6 +56,56 @@ public class MainActivity extends AppCompatActivity {
   private void loginUser() {
     loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID,-1);
   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logged_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+      MenuItem item = menu.findItem(R.id.logoutMenuItem);
+      item.setVisible(true);
+      item.setTitle("Log out");
+      item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(@NonNull MenuItem item) {
+          showLogoutDialog();
+          return false;
+        }
+      });
+        return true;
+    }
+
+  private void showLogoutDialog(){
+    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+    final AlertDialog alertDialog = alertBuilder.create();
+
+    alertBuilder.setMessage("Logout?");
+
+    alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        loggout();
+      }
+    });
+
+    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        alertDialog.dismiss();
+      }
+    });
+
+    alertBuilder.create().show();
+
+  }
+  private void loggout() {
+    startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+  }
+
 
   static Intent mainActivityIntentFactory(Context context, int userId){
     Intent intent = new Intent(context, MainActivity.class);
