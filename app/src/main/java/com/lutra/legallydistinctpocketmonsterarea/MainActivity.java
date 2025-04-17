e40e1cbb55a8fd15a5419767cef6c1f947a6043e
Victor Gordian <victorgordian@hotmail.com>
2025-04-16 17:03:30 -0700

    connected the login to be first screen

--- app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MainActivity.java
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MainActivity.java
@@ -1,5 +1,7 @@
 package com.lutra.legallydistinctpocketmonsterarea;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +12,24 @@ import com.lutra.legallydistinctpocketmonsterarea.databinding.ActivityMainBindin
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String MAIN_ACTIVITY_USER_ID = "com.lutra.legallydistinctpocketmonsterarea.MAIN_ACTIVITY_USER_ID";
   private ActivityMainBinding binding;
   private AppRepository repository;
+
+  int loggedInUserId = -1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
+
+    loginUser();
+
+    if(loggedInUserId == -1 ){
+      Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+      startActivity(intent);
+    }
 
     repository = AppRepository.getRepository(getApplication());
 
@@ -28,5 +40,15 @@ public class MainActivity extends AppCompatActivity {
     String text = monsterTypes == null ? "null" : monsterTypes.stream().map(MonsterType::toString).collect(Collectors.joining("\n\n"));
     String text2 = userMonsters == null ? "null" : userMonsters.stream().map(UserMonster::toString).collect(Collectors.joining("\n\n"));
     binding.myTextView.setText(text + "\n\n\n\n" + text2);
+  }
+
+  private void loginUser() {
+    loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID,-1);
+  }
+
+  static Intent mainActivityIntentFactory(Context context, int userId){
+    Intent intent = new Intent(context, MainActivity.class);
+    intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+    return intent;
   }
 }
