49abf02e8bffa18b8c5a000d7be9685f27b9472e
qzmp010 <66999686+qzmp010@users.noreply.github.com>
2025-04-19 17:01:07 -0700

    update ViewMonsters recycler view to use MonsterTypeWithUserMonsters; todo: display each UserMonster separately

--- app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/database/entities/MonsterTypeWithUserMonsters.java
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/database/entities/MonsterTypeWithUserMonsters.java
@@ -3,6 +3,7 @@ package com.lutra.legallydistinctpocketmonsterarea.database.entities;
 import androidx.room.Embedded;
 import androidx.room.Relation;
 import java.util.List;
+import java.util.Objects;
 
 public class MonsterTypeWithUserMonsters {
   @Embedded public MonsterType monsterType;
@@ -18,5 +19,20 @@ public class MonsterTypeWithUserMonsters {
         "monsterType=" + monsterType +
         ", userMonsters=" + userMonsters +
         '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MonsterTypeWithUserMonsters that = (MonsterTypeWithUserMonsters) o;
+    return Objects.equals(monsterType, that.monsterType) && Objects.equals(
+        userMonsters, that.userMonsters);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(monsterType, userMonsters);
   }
 }
