12530810f4501605409ffee7c828adb20b4a2aaa
discotechnology <drosenfeld@csumb.edu>
2025-04-21 16:36:05 -0700

    Created function MonsterFactory.createNewMonster() to insert new monster into DB with assigned stats.

--- app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MonsterFactory.java
+++ app/src/main/java/com/lutra/legallydistinctpocketmonsterarea/MonsterFactory.java
@@ -19,6 +19,7 @@ public abstract class MonsterFactory {
 
     private static final String TAG = "MonsterFactory.java";
     private static final int DEFAULT_USER = -1;
+    private static final int DEFAULT_ID = 0;
 
     static UserMonster getRandomMonster(AppRepository repository) {
 
@@ -51,7 +52,7 @@ public abstract class MonsterFactory {
                 Math.abs(rand.nextInt() % (template.getDefenseMax() - template.getDefenseMin()));
 
         return new UserMonster(
-                Math.abs(rand.nextInt()),
+                DEFAULT_ID,
                 template.getMonsterTypeName(),
                 template.getPhrase(),
                 template.getImageID(),
@@ -62,6 +63,65 @@ public abstract class MonsterFactory {
                 DEFAULT_USER,
                 template.getMonsterTypeId()
         );
+    }
+
+    /**
+     * Use this function to insert a monster with known characteristics into the database. Order of params listed below.
+     * @param repository A good instance of the repository.
+     * @param monsterTypeID Type ID of monster to instantiate.
+     * @param nickname Use "" as default.
+     * @param phrase Use "" as default.
+     * @param attack Suggested range between 5 (very weak) to 15 (very strong)
+     * @param defense Suggested range between 3 (very weak) to 10 (very strong)
+     * @param health Suggested range between 15 (very weak) to 50 (very strong)
+     * @param userID User to assign monster to.
+     * @return True if monster was able to be created.
+     */
+    public static boolean createNewMonster(AppRepository repository, int monsterTypeID, String nickname, String phrase, int attack, int defense, int health, int userID) {
+        MonsterType template;
+        do {
+            try {
+                template = repository.getMonsterTypeById(monsterTypeID);
+            } catch (RuntimeException e) {
+                Log.e(TAG, "Unable to retrieve template monster.");
+                return false;
+            }
+        } while(template == null);
+
+        if(nickname.isEmpty()) {
+            nickname = template.getMonsterTypeName();
+        }
+
+        if(phrase.isEmpty()) {
+            phrase = template.getPhrase();
+        }
+
+        //The following checks make sure it's not possible to create a monster that can't battle.
+        if(attack < 0) {
+            attack = 1;
+        }
+
+        if(defense < 0) {
+            defense = 1;
+        }
+
+        if(health < 0) {
+            health = 1;
+        }
+
+        repository.insertUserMonster(new UserMonster(
+                DEFAULT_ID,
+                nickname,
+                phrase,
+                template.getImageID(),
+                template.getElementalType(),
+                attack,
+                defense,
+                health,
+                userID,
+                template.getMonsterTypeId()
+        ));
+        return true;
     }
 
     public static UserMonster getUserMonster(AppRepository repository, int userID) {
